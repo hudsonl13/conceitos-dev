94cb8cbf8f8c04217c8ec6daf413cf9e0b5a7c86
Hudson Lessa <admin@hudsonlessa.com>
2020-09-15 18:32:55 -0300

    Initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import Header from './components/Header';
+import './App.css';
+// import backgroundImage from './assets/background.jpg';
+import api from './services/api';
+
+function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject()
+  {
+    // setProjects([...projects, `Novo projeto: ${Date.now()}`]);
+    const response = await api.post('/projects', {
+      title: `Novo projeto: ${Date.now()}`,
+      owner: 'eu'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <Header title='Projects' />
+
+      {/* <img width={300} src={backgroundImage} /> */}
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  );
+}
+
+export default App;
